4e5e00d500e24e06b1726ba01a4d62db5f0c74c1
ronsha001 <ronsh0111@gmail.com>
2022-04-11 17:58:21 +0300

    git ignore

--- loginSystem/db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    define("HOST", "localhost");
-    define("USER", "root");
-    define("PASSWORD", "");
-    define("DB", "getagent");
-    //////
-    $con = mysqli_connect(HOST, USER, PASSWORD, DB);
-    mysqli_set_charset($con, "utf8");
-?>
